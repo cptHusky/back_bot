cdb8bc66e0fa5b8b9059fef84abbe1ef61d733f2
pealekseev <pealekseev@ptsecurity.com>
2024-01-09 19:26:50 +0300

    added exceptions and logging, ready to launch

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
+import logging
+import os
 from datetime import datetime, time, timedelta
+from logging.handlers import TimedRotatingFileHandler
 from random import randint
 from time import sleep
 
-from psycopg import connect
+from psycopg import DatabaseError, connect
 from requests import post
 
 from bot_config import BOT_TOKEN, CHAT_ID
 from db_config import DB_HOST, DB_NAME, DB_PASS, DB_USER
 from phrases import phrases
 
+RETRY_TIMEOUT = 15
+MAX_RETRIES = 10
+
 START_DATETIME = datetime.combine(datetime.now().date(), time(7, 0))
 END_DATETIME = datetime.combine(datetime.now().date(), time(22, 0))
+
 URL_WO_TEXT = f'https://api.telegram.org/bot{BOT_TOKEN}/sendMessage?chat_id={CHAT_ID}'
+
 ID_COLUMN_ID = 0
+DB_ERROR_TEXT = "У меня пока нет для тебя мотивирующей фразы, просто выпрями спину."
 
 
 def get_sleep_time() -> int:
@@ -66,15 +75,70 @@ def get_text() -> str:
     return phrases[phrase_id]
 
 
+def send_message(text: str) -> int:
+    url = f'{URL_WO_TEXT}&text={text}'
+    logging.info(f"POSTing: \"{url}\"")
+    code = post(url).status_code
+    logging.info(f"POST returned: {code}")
+    return code
+
+
 def main():
+    logging.info("Bot started")
     sleep_time = 0
     while True:
-        sleep(sleep_time)
-        text = get_text()
-        url = f'{URL_WO_TEXT}&text={text}'
-        post(url)
-        sleep_time = get_sleep_time()
+        try:
+            sleep(sleep_time)
+
+            try:
+                text = get_text()
+            except DatabaseError as e:
+                logging.error(f"Database error: {e}")
+                text = DB_ERROR_TEXT
+
+            code = send_message(text=text)
+            if code == 200:
+                logging.info(f"Message sent: \"{text}\"")
+            else:
+                logging.error(f"Request error, returned {code}")
+
+                for attempt_number in range(MAX_RETRIES):
+                    logging.info(f"Retry {attempt_number + 1} out of {MAX_RETRIES} in {RETRY_TIMEOUT} seconds...")
+                    sleep(RETRY_TIMEOUT)
+                    send_message(text=text)
+                    if code == 200:
+                        logging.info(f"Message sent after retry: \"{text}\"")
+                        break
+                    else:
+                        logging.error(f"Retry {attempt_number + 1} failed, returned code {code}")
+
+            sleep_time = get_sleep_time()
+            next_message_time = datetime.now() + timedelta(seconds=sleep_time)
+            logging.info(f"Next message will be sent at {next_message_time}")
+
+        except Exception as e:
+            logging.error(f"Unexpected: {e}")
+
+
+def logger_init():
+    log_directory = "logs"
+    os.makedirs(log_directory, exist_ok=True)
+    log_file = os.path.join(log_directory, "bot_log")
+
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+
+    handler = TimedRotatingFileHandler(
+        filename=log_file,
+        when="midnight",
+        interval=1
+    )
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+
+    logger.addHandler(handler)
 
 
 if __name__ == '__main__':
+    logger_init()
     main()
